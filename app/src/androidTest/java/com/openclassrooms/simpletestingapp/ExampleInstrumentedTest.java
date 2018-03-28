8db17572fa05852f31a77b8c67c4a7c5f6c687c3
Boisney Philippe <phil.boisney@gmail.com>
2018-03-28 09:10:15 +0200

    First Commit

---
+++ app/src/androidTest/java/com/openclassrooms/simpletestingapp/ExampleInstrumentedTest.java
@@ -0,0 +1,43 @@
+package com.openclassrooms.simpletestingapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.core.StringContains.containsString;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void useAppContext() throws Exception {
+        Context appContext = InstrumentationRegistry.getTargetContext();
+        assertEquals("com.openclassrooms.simpletestingapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void checkIfTextViewContainsText(){
+        onView(withId(R.id.activity_main_text_view)).check(matches(withText(containsString("Hello World!"))));
+    }
+}
